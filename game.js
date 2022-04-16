c697ba136a68dc9dc20d4605321dd1d82a050e9a
zaranah <zaranahier@gmail.com>
2022-04-16 15:31:05 +0900

    game完了ページJS記述

--- game.js
+++ game.js
@@ -55,8 +55,9 @@ startGame = () => {
 getNewQuestion = () => {
 
   if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score);
     //go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
 
   questionCounter++;
