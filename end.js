15fa229123e1a1cc8c795e850e62404655d93b60
zaranah <zaranahier@gmail.com>
2022-04-22 11:58:03 +0900

    score保存値変更

--- end.js
+++ end.js
@@ -18,7 +18,8 @@ saveHighScore = (e) => {
   e.preventDefault();
 
   const score = {
-    score: Math.floor(Math.random()*100),
+    // score: Math.floor(Math.random()*100),
+    score: mostRecentScore,
     name: username.value
   };
   highScores.push(score);
